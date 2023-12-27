10eb925eaae3fdfd2be121ea1fb805edbb1da4e3
akashhaxor8 <akashhaxor8@gmail.com>
2023-12-27 21:14:51 +0530

    Casting demo done

--- demo1.py
+++ demo1.py
@@ -11,8 +11,6 @@ print ("My Name is:",name)
 print ("My age is:" ,age)
 print ("My Address is:" ,address)
 
-"""
-
 a=int(input())
 b=int(input())
 c=int(input())
@@ -20,3 +18,14 @@ M=(a*b*c)
 add=(a+b+c)
 f=M/add
 print(f)
+
+"""
+name=input()
+score= int(input())
+dept=input()
+M=(score/10)
+print("My name is:", name)
+print("My score is:", M,"/10")
+print("My Department is:", dept)
+
+
