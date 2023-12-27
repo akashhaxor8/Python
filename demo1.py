67cfd32a522b01405f2110afbf4c297b126188bd
akashhaxor8 <akashhaxor8@gmail.com>
2023-12-27 20:54:48 +0530

    new demo added

--- demo1.py
+++ demo1.py
@@ -1,4 +1,22 @@
+"""
 name=input()
 age=int(input())
 print ("My Name is:",name)
 print ("My age is:" ,age)
+
+name=input()
+age=int(input())
+address=input()
+print ("My Name is:",name)
+print ("My age is:" ,age)
+print ("My Address is:" ,address)
+
+"""
+
+a=int(input())
+b=int(input())
+c=int(input())
+M=(a*b*c)
+add=(a+b+c)
+f=M/add
+print(f)
